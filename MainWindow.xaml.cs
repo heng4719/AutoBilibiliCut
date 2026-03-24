cfa32a05f2baa17f903ba6836aa159c41718a227
ZhangHeng <986472954@qq.com>
2026-03-24 15:19:04 +0800

    优化窗口样式

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Input;
+using FontAwesome.WPF;
 using videoCut.Services;
 using videoCut.ViewModels;
 
@@ -13,5 +15,61 @@ public partial class MainWindow : Window
             new VideoMetadataService(),
             new VideoExportService(),
             new YtDlpDownloadService());
+        UpdateWindowStateButton();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton != MouseButton.Left)
+        {
+            return;
+        }
+
+        DragMove();
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void ToggleWindowStateButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Window_StateChanged(object sender, EventArgs e)
+    {
+        UpdateWindowStateButton();
+    }
+
+    private void UpdateWindowStateButton()
+    {
+        if (ToggleWindowStateButton is null)
+        {
+            return;
+        }
+
+        var icon = new ImageAwesome
+        {
+            Icon = WindowState == WindowState.Maximized ? FontAwesomeIcon.WindowRestore : FontAwesomeIcon.WindowMaximize,
+            Width = 14,
+            Height = 14
+        };
+
+        icon.SetBinding(ImageAwesome.ForegroundProperty, new System.Windows.Data.Binding("Foreground")
+        {
+            Source = ToggleWindowStateButton
+        });
+
+        ToggleWindowStateButton.Content = icon;
+        ToggleWindowStateButton.ToolTip = WindowState == WindowState.Maximized ? "还原" : "最大化";
     }
 }
